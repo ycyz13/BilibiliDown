9a08db3512e9907b79ee7cb0dd2fedce74268812
ycyz13 <ycyz13@gmail.com>
2024-08-23 19:20:15 +0800

    新建vedio表
    (cherry picked from commit 3515506fc16c052c3025e23ccc6d1cdb2d0762aa)

---
+++ sql/建表语句.sql
@@ -0,0 +1,31 @@
+CREATE TABLE up (
+    id bigint(20) AUTO_INCREMENT NOT NULL COMMENT '主键',
+    biz_no varchar(16) NOT NULL COMMENT '编号',
+    uid varchar(64) NOT NULL COMMENT 'up主用户id',
+    up_name varchar(32) NOT NULL COMMENT 'up名称',
+    handle_status int(3) NOT NULL COMMENT '处理状态。1：初始化，2：常规同步',
+    last_sync_time datetime NOT NULL COMMENT '上次成功同步作品时间',
+    update_time datetime NOT NULL COMMENT '更新记录时间',
+    create_time datetime NOT NULL COMMENT '创建记录时间',
+    PRIMARY KEY (id),
+    UNIQUE KEY uniq_biz_no(biz_no),
+    UNIQUE KEY uniq_uid(uid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='up主信息维护表'
+
+CREATE TABLE vedio (
+    id bigint(20) AUTO_INCREMENT NOT NULL COMMENT '主键',
+    up_uid varchar(64) NOT NULL COMMENT 'up主用户id',
+    handle_status int(3) NOT NULL COMMENT '处理状态。1：待同步，2：同步完毕',
+    bv_id varchar(64) NOT NULL COMMENT 'bvId',
+    cid varchar(64) NOT NULL COMMENT 'cid',
+    av_title varchar(128) NOT NULL COMMENT 'av标题',
+    title varchar(128) NOT NULL COMMENT '标题',
+    file_name varchar(128) NOT NULL COMMENT '下载文件名称',
+    store_path varchar(1024) NOT NULL COMMENT '文件存储路径',
+    qn int NOT NULL COMMENT '视频清晰度',
+    update_time datetime NOT NULL COMMENT '更新记录时间',
+    create_time datetime NOT NULL COMMENT '创建记录时间',
+    PRIMARY KEY (id),
+    UNIQUE KEY uniq_bv_id(bv_id),
+    UNIQUE KEY uniq_up_uid(up_uid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='投稿表'
