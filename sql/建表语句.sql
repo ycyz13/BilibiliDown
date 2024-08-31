a70a56e7ca535cf396e0c387d19c45ea85093c68
ycyz13 <ycyz13@gmail.com>
2024-09-01 02:17:28 +0800

    修改建表语句
    (cherry picked from commit 2188202aca654ba957b9b00839b1f4aaf5e48ac2)

--- sql/建表语句.sql
+++ sql/建表语句.sql
@@ -11,12 +11,14 @@ CREATE TABLE up (
     UNIQUE KEY uniq_biz_no(biz_no),
     UNIQUE KEY uniq_uid(uid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='up主信息维护表'
+// todo: 给up表增加自动update time
 
 CREATE TABLE vedio (
     id bigint(20) AUTO_INCREMENT NOT NULL COMMENT '主键',
     up_uid varchar(64) NOT NULL COMMENT 'up主用户id',
     handle_status int NOT NULL COMMENT '处理状态。1：待同步，2：同步完毕',
     bv_id varchar(64) NOT NULL COMMENT 'bvId',
+    bv_create_time datetime NOT NULL COMMENT '投稿时间',
     cid varchar(64) NOT NULL COMMENT 'cid',
     bv_page int NOT NULL COMMENT '投稿内的选集',
     av_title varchar(128) NOT NULL COMMENT 'av标题',
@@ -24,8 +26,8 @@ CREATE TABLE vedio (
     file_name varchar(128) NOT NULL COMMENT '下载文件名称',
     store_path varchar(1024) NOT NULL COMMENT '文件存储路径',
     qn int NOT NULL COMMENT '视频清晰度',
-    update_time datetime NOT NULL COMMENT '更新记录时间',
-    create_time datetime NOT NULL COMMENT '创建记录时间',
+    update_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新记录时间',
+    create_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建记录时间',
     PRIMARY KEY (id),
     UNIQUE KEY uniq_bv_id_cid(bv_id, cid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='投稿表'
